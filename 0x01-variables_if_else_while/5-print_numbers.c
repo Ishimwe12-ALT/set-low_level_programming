5cbbcc4376dc8d3127003ff9e32ebb1d46c161fa
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-03-02 10:21:04 -0800

    Complete 0x01-variables_if_else_while: Tasks 0-9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    for (n = 0; n < 10; n++)
-    {
-        printf("%d", n);
-    }
-    printf("\n");
-
-    return (0);
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	
+	return (0);
 }
-
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    for (n = 0; n < 10; n++)
-    {
-        putchar('0' + n);
-    }
-    putchar('\n');
-
-    return (0);
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+	}
+	putchar('\n');
+	
+	return (0);
 }
-
