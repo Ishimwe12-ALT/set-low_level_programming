c8a6f2fd79d9e2dd7e66eeca28173968a480d54a
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-03-15 00:22:00 +0200

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,28 +1,30 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-/* Task 0 */
-char *_strcat(char *dest, char *src);
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
 
-/* Task 1 */
-char *_strncat(char *dest, char *src, int n);
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-/* Task 2 */
-char *_strncpy(char *dest, char *src, int n);
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-/* Task 3 */
-int _strcmp(char *s1, char *s2);
+	dest[i] = '\0';
 
-/* Task 4 */
-void reverse_array(int *a, int n);
-
-/* Task 5 */
-char *string_toupper(char *);
-
-/* Task 6 */
-char *cap_string(char *);
-
-/* Task 7 */
-char *leet(char *);
-
-#endif
+	return (dest);
+}
