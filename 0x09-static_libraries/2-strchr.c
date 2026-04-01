0813c41260536487b32b97595fa040d91723b501
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-04-01 13:04:45 +0200

    Create 2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to locate
+ *
+ * Return: pointer to first occurrence or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	if (c == '\0')
+	{
+		return (s);
+	}
+
+	return (0);
+}
