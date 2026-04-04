f60ebb2aa30c7492c34a81a6ca4efdb7cd663c5b
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-04-05 00:46:47 +0200

    Refactor argument count printing in main function

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,27 +1,38 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: number
- */
-void print_number(int n)
-{
-	if (n >= 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
-}
-
-/**
- * main - prints number of arguments passed
+ * main - prints number of arguments
  * @argc: argument count
  * @argv: argument vector
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
+	int n = argc - 1;
+
 	(void)argv;
 
-	print_number(argc - 1);
+	if (n == 0)
+		_putchar('0');
+	else if (n == 1)
+		_putchar('1');
+	else if (n == 2)
+		_putchar('2');
+	else if (n == 3)
+		_putchar('3');
+	else if (n == 4)
+		_putchar('4');
+	else if (n == 5)
+		_putchar('5');
+	else if (n == 6)
+		_putchar('6');
+	else if (n == 7)
+		_putchar('7');
+	else if (n == 8)
+		_putchar('8');
+	else if (n == 9)
+		_putchar('9');
+
 	_putchar('\n');
 
 	return (0);
