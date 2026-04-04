c0775b7f0775b3d025140dfebf0e19be0f689510
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-04-05 00:42:57 +0200

    Fix argument count display in main function
    Updated printf to correctly display the number of arguments.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints the number of arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
 	(void)argv;
