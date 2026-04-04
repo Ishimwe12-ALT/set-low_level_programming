5c000d5f396fe030bd8bd97276f7703a9e9ed78a
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-04-05 00:49:10 +0200

    Refactor number printing for argument count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,27 +12,11 @@ int main(int argc, char *argv[])
 
 	(void)argv;
 
-	if (n == 0)
-		_putchar('0');
-	else if (n == 1)
-		_putchar('1');
-	else if (n == 2)
-		_putchar('2');
-	else if (n == 3)
-		_putchar('3');
-	else if (n == 4)
-		_putchar('4');
-	else if (n == 5)
-		_putchar('5');
-	else if (n == 6)
-		_putchar('6');
-	else if (n == 7)
-		_putchar('7');
-	else if (n == 8)
-		_putchar('8');
-	else if (n == 9)
-		_putchar('9');
-
+	if (n >= 10)
+	{
+		_putchar((n / 10) + '0');
+	}
+	_putchar((n % 10) + '0');
 	_putchar('\n');
 
 	return (0);
