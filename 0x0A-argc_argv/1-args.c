e701b3728ae9f9a6782d0ba4cc72bcac3d005927
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-04-11 00:13:14 +0200

    Refactor main function to print argument count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
-#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 
 /**
  * main - prints the number of arguments passed into it
@@ -8,16 +16,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	int count = argc - 1;
+	(void)argv;
 
-	if (count >= 10)
-	{
-		/* This handles two digit numbers if necessary */
-		_putchar((count / 10) + '0');
-	}
-	_putchar((count % 10) + '0');
+	_putchar((argc - 1) + '0');
 	_putchar('\n');
 
 	return (0);
