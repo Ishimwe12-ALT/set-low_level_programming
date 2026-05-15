949a408a2a1f5707f18de9546765909ac5dab04a
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-05-15 11:37:29 -0700

    Complete more_malloc_free: Tasks 0-3

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes from s2 to concatenate
+ *
+ * Return: pointer to newly allocated space with concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int i, j, len1, len2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = 0;
+	while (s1[len1])
+		len1++;
+
+	len2 = 0;
+	while (s2[len2])
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+
+	concat = malloc((len1 + n + 1) * sizeof(char));
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
+
+	concat[i + j] = '\0';
+
+	return (concat);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,2 @@
+# more_malloc_free - C Programming
+This project covers advanced dynamic memory allocation techniques in C.
