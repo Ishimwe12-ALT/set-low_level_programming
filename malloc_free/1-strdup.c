e7cf4da9c7165456efe676e61322066ae11179a7
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-05-15 10:57:51 -0700

    Complete malloc_free: Tasks 0-4

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string, or NULL if it fails to duplicate str
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (str[len])
+		len++;
+
+	dup = malloc((len + 1) * sizeof(char));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		dup[i] = str[i];
+	}
+
+	return (dup);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,2 @@
+# malloc_free - C Programming
+This project covers dynamic memory allocation with malloc and free in C.
