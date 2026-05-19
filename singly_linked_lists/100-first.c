8f3cb9aa3988f4183fcacf940908997f7b6b2f35
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-05-19 14:07:42 +0200

    Implement tortoise function to print a message
    Add a constructor function that prints a message before main.

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * tortoise - prints before main function
+ */
+void __attribute__((constructor)) tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
