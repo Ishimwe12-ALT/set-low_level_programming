932e3752d54f9372dc41961b7064eb51b5d0f6a0
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-05-25 00:03:28 +0200

    Refactor insert_nodeint_at_index for clarity

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -2,34 +2,34 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given index
- * @head: pointer to the first node
- * @idx: index where the new node should be added
- * @n: value of the new node
+ * insert_nodeint_at_index - inserts a node at index
+ * @head: pointer to pointer to first node
+ * @idx: index to insert at
+ * @n: value of new node
  *
- * Return: address of the new node, or NULL if it failed
+ * Return: address of new node or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head,
 	unsigned int idx, int n)
 {
-	listint_t *new_node;
+	listint_t *new;
 	listint_t *temp;
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(listint_t));
+	if (!new)
 		return (NULL);
 
-	new_node->n = n;
+	new->n = n;
 
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
 
 	temp = *head;
@@ -40,14 +40,14 @@ listint_t *insert_nodeint_at_index(listint_t **head,
 		i++;
 	}
 
-	if (temp == NULL)
+	if (!temp)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
 
-	new_node->next = temp->next;
-	temp->next = new_node;
+	new->next = temp->next;
+	temp->next = new;
 
-	return (new_node);
+	return (new);
 }
