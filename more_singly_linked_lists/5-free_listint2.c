16b3c0e3f576c5bb58841af72ee7f5ca3ed07b60
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-05-25 00:09:32 +0200

    Create 5-free_listint2.c

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to pointer to first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
