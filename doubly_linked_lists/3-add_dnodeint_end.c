8d300a45683eab5304f3ce125786eaccaabe2572
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-05-30 14:01:56 +0200

    Add function to append node at end of doubly linked list
    Implemented the function to add a new node at the end of a doubly linked list.

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,31 +1,40 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stddef.h>
+#include "lists.h"
+#include <stdlib.h>
 
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+ * @head: double pointer to the head of the list
+ * @n: the integer to add to the new node
  *
- * Description: doubly linked list node structure
+ * Return: address of the new element, or NULL if it failed
  */
-typedef struct dlistint_s
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
-} dlistint_t;
+	dlistint_t *new_node, *temp;
 
-size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head);
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+	if (head == NULL)
+		return (NULL);
 
-#endif /* LISTS_H */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new_node;
+	new_node->prev = temp;
+
+	return (new_node);
+}
