77146dbe5e18c83fe13c8b7e968a69e33469d3d4
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-05-30 13:41:32 +0200

    Refactor memory management in list freeing functions
    Added a new function _realloc_free to handle memory reallocation for node pointers and updated free_listint_safe to use it.

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,68 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint2 - Frees a listint_t list and sets the head to NULL.
- * @head: Double pointer to the head of the listint_t list.
+ * _realloc_free - reallocates memory for an array of node pointers
+ * @list: the old list
+ * @size: size of the new list
+ * @new_node: new node to add
+ *
+ * Return: pointer to the new list
  */
-void free_listint2(listint_t **head)
+listint_t **_realloc_free(listint_t **list, size_t size, listint_t *new_node)
 {
-	listint_t *temp;
+	listint_t **new_list;
+	size_t i;
 
-	if (head == NULL)
-		return;
-
-	while (*head != NULL)
+	new_list = malloc(size * sizeof(listint_t *));
+	if (new_list == NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		free(list);
+		exit(98);
 	}
 
-	*head = NULL;
+	for (i = 0; i < size - 1; i++)
+		new_list[i] = list[i];
+
+	new_list[i] = new_node;
+	free(list);
+
+	return (new_list);
+}
+
+/**
+ * free_listint_safe - safely frees a listint_t list.
+ * @h: double pointer to the head of the list
+ *
+ * Return: the size of the list that was free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t i, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	while (*h != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (*h == list[i])
+			{
+				*h = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _realloc_free(list, num, *h);
+		next = (*h)->next;
+		free(*h);
+		*h = next;
+	}
+
+	free(list);
+	return (num);
 }
