b25520a8f3d54a2e80b9292836fb2febf0cab725
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-05-30 13:45:26 +0200

    Enhance print_listint_safe with dynamic memory allocation
    Refactor print_listint_safe to use dynamic memory for visited nodes and improve safety.

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,37 +1,65 @@
 #include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * print_listint_safe - prints a listint_t list safely
- * @head: pointer to head
- * Return: number of nodes
+ * _realloc_ptrs - reallocates memory for an array of node pointers
+ * @list: the old list to append to
+ * @size: size of the new list
+ * @new_node: new node to add to the array
+ *
+ * Return: pointer to the new list
+ */
+const listint_t **_realloc_ptrs(const listint_t **list, size_t size,
+				const listint_t *new_node)
+{
+	const listint_t **new_list;
+	size_t i;
+
+	new_list = malloc(size * sizeof(listint_t *));
+	if (new_list == NULL)
+	{
+		free((void *)list);
+		exit(98);
+	}
+
+	for (i = 0; i < size - 1; i++)
+		new_list[i] = list[i];
+
+	new_list[i] = new_node;
+	free((void *)list);
+
+	return (new_list);
+}
+
+/**
+ * print_listint_safe - safely prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t **visited;
-    size_t count = 0, i;
+	size_t i, num = 0;
+	const listint_t **list = NULL;
 
-    visited = malloc(sizeof(listint_t *) * 1024);
-    if (!visited)
-        exit(98);
+	while (head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free((void *)list);
+				return (num);
+			}
+		}
+		num++;
+		list = _realloc_ptrs(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
 
-    while (head)
-    {
-        for (i = 0; i < count; i++)
-        {
-            if (head == visited[i])
-            {
-                printf("-> [%p] %d\n", (void *)head, head->n);
-                free(visited);
-                return (count);
-            }
-        }
-
-        printf("[%p] %d\n", (void *)head, head->n);
-        visited[count++] = head;
-        head = head->next;
-    }
-
-    free(visited);
-    return (count);
+	free((void *)list);
+	return (num);
 }
