7c5b637448f03d7120d0adb26b7f830a2d9ac66c
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-06-26 18:46:46 +0200

    Add hash_table_print function to display hash table
    Implements a function to print key/value pairs in a hash table.

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table to print
+ *
+ * Description: Prints the key/value pairs in the hash table in the
+ * order they appear (array then list). Format: {'key': 'value', ...}
+ *
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	int first = 1;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
