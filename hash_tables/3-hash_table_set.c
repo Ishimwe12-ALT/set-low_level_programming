44e6faefde6247832c21f944a7c841eff0d93863
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-08-14 09:38:36 -0700

    Split node creation into static helper function to fix Betty 40 line limit

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,58 +3,78 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - Creates a new hash node
+ * @key: The key for the node
+ * @value: The value for the node
+ *
+ * Return: Pointer to new node, or NULL on failure
+ */
+static hash_node_t *create_node(const char *key, const char *value)
+{
+hash_node_t *node = malloc(sizeof(hash_node_t));
+
+if (!node)
+return (NULL);
+
+node->key = strdup(key);
+if (!node->key)
+{
+free(node);
+return (NULL);
+}
+
+node->value = strdup(value);
+if (!node->value)
+{
+free(node->key);
+free(node);
+return (NULL);
+}
+
+node->next = NULL;
+return (node);
+}
+
+/**
  * hash_table_set - Adds or updates an element in the hash table
- * @ht: The hash table to add or update key/value
- * @key: The key (cannot be empty)
- * @value: The value associated with the key (must be duplicated)
+ * @ht: The hash table
+ * @key: The key
+ * @value: The value
  *
  * Return: 1 if succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 hash_node_t *new_node, *tmp;
-char *value_copy;
-unsigned long int index;
+char *val_copy;
+unsigned long int idx;
 
-if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+if (!ht || !key || !*key || !value)
 return (0);
 
-value_copy = strdup(value);
-if (value_copy == NULL)
-return (0);
+idx = key_index((const unsigned char *)key, ht->size);
+tmp = ht->array[idx];
 
-index = key_index((const unsigned char *)key, ht->size);
-tmp = ht->array[index];
-
-while (tmp != NULL)
+while (tmp)
 {
 if (strcmp(tmp->key, key) == 0)
 {
+val_copy = strdup(value);
+if (!val_copy)
+return (0);
 free(tmp->value);
-tmp->value = value_copy;
+tmp->value = val_copy;
 return (1);
 }
 tmp = tmp->next;
 }
 
-new_node = malloc(sizeof(hash_node_t));
-if (new_node == NULL)
-{
-free(value_copy);
+new_node = create_node(key, value);
+if (!new_node)
 return (0);
-}
 
-new_node->key = strdup(key);
-if (new_node->key == NULL)
-{
-free(value_copy);
-free(new_node);
-return (0);
-}
-
-new_node->value = value_copy;
-new_node->next = ht->array[index];
-ht->array[index] = new_node;
+new_node->next = ht->array[idx];
+ht->array[idx] = new_node;
 
 return (1);
 }
